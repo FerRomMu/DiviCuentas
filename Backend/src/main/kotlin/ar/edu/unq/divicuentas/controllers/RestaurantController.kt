fc954c8b6bf7713e18e3252dfee4f5b1d0c91580
Fernando Romero <fer.rom.mu@gmail.com>
2023-09-24 13:30:41 -0300

    Feat: Controller devuelve todos los restaurantes.

--- Backend/src/main/kotlin/ar/edu/unq/divicuentas/controllers/RestaurantController.kt
+++ Backend/src/main/kotlin/ar/edu/unq/divicuentas/controllers/RestaurantController.kt
@@ -2,13 +2,11 @@ package ar.edu.unq.divicuentas.controllers
 
 import ar.edu.unq.divicuentas.mapping.RestaurantMapper
 import ar.edu.unq.divicuentas.mapping.dto.RestaurantDTO
+import ar.edu.unq.divicuentas.modelo.Restaurant
 import ar.edu.unq.divicuentas.service.IRestaurantService
 import javassist.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 @ServiceREST
 @RequestMapping("/restaurant")
@@ -19,8 +17,8 @@ class RestaurantController {
     @Autowired
     lateinit var restaurantMapper: RestaurantMapper
 
-    @GetMapping
-    fun restaurant(id: Long) : RestaurantDTO {
+    @GetMapping("/{id}")
+    fun restaurant(@PathVariable id: Long) : RestaurantDTO {
         val restaurant = service.getById(id) ?: throw NotFoundException("Restaurante no encontrado.")
         return restaurantMapper.toDTO(restaurant)
     }
@@ -28,5 +26,10 @@ class RestaurantController {
     @PostMapping
     fun createRestaurant(@RequestBody restaurantDTO: RestaurantDTO): RestaurantDTO {
         return restaurantMapper.toDTO(service.create(restaurantMapper.toModel(restaurantDTO)))
+    }
+
+    @GetMapping("/all")
+    fun restaurants(): List<RestaurantDTO>{
+        return service.restaurants().map { restaurant -> restaurantMapper.toDTO(restaurant) }
     }
 }
