4037af5bed7654a37ff3b63255ecdc7b2b09e6a6
Fernando Romero <fer.rom.mu@gmail.com>
2023-10-05 20:32:10 -0300

    Refactor: Se terminan de desacoplar cosas.

--- Frontend/src/components/menu/Menu.jsx
+++ Frontend/src/components/menu/Menu.jsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import ModalCrearPedido from './modal-crear-pedido/ModalCrearPedido';
 import useProductsData from '../../hooks/useProductsData';
 import ProductsRow from './products-row/productsRow';
+import ModalVerDetalles from './modal-ver-detalles/ModalVerDetalles';
 
 const Menu = () => {
 
@@ -15,11 +16,13 @@ const Menu = () => {
     const products = useProductsData(restaurant?.menu);
     
     const [openModal, setOpenModal] = useState(false);
+    const [openDetails, setOpenDetails] = useState(false);
     const [isOrder, setOrder] = useState(false);
     const [name, setName] = useState("");
 
     const productsInRows = (products) => {
       if(products === null){ return [] }
+
       const productsInRows = []
       for (let i = 0; i < products.length; i += 3) {
         productsInRows.push(products.slice(i,i+3))
@@ -34,35 +37,53 @@ const Menu = () => {
     const abrirModal = () => {
       setOpenModal(true);
     }
- 
-    const list = productsInRows(products)
 
+    const abrirDetalles = () => {
+      setOpenDetails(true);
+    }
 
+    const order = () => {
+      return { 
+        'owner': name,
+        'products': products.map(product => ({
+          ...product,
+          amount: () => 1
+        }))
+      }
+    }
 
     return (
         <main className='container'>
+          <header>
             <h1 className='title_menu'>Menú</h1>
             <button className='volver-btn' onClick={() => backToHome()}> Volver </button>
             {isOrder && (
               <h2>Está pidiendo: {name} </h2>
             )}
-            <div>
+          </header>
+          <section className='products'>
             {
-              list.map((productsTrio, i) => {
-                return <section className='products'>{
-                  <ProductsRow products={productsTrio} isOrder={isOrder}/>
-                }</section>
+              productsInRows(products).map((productsTrio, i) => {
+                return <ProductsRow products={productsTrio} isOrder={isOrder}/>
               })
-            }</div>
-            {!isOrder && (
-            <div>
+            }
+          </section>
+          <footer>
+            {isOrder? 
+              <>
+                <button onClick={abrirDetalles}>Ver pedido</button>
+                {openDetails && (
+                  <ModalVerDetalles order={order()} restaurant={restaurant}/>
+                )}
+              </> : 
+              <>
                 <button onClick={abrirModal}>Crear Pedido</button>
                 {openModal && (
-                  <ModalCrearPedido setOrder={setOrder} setName={setName} ></ModalCrearPedido>
+                  <ModalCrearPedido setOrder={setOrder} setName={setName}/>
                 )}
-            </div>
-            )}
-
+              </>
+            }
+          </footer>
         </main>
     )
 
