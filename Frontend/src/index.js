81ebbf83473bcbf9eb26f36aeebc67f6b1aabf02
Fernando Romero <fer.rom.mu@gmail.com>
2023-10-06 18:27:06 -0300

    Refactor: Desacoplo chakra y agrego estilos.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import theme from "./theme";
+import "./theme.css";
 import App from './App';
 import { DatabaseProvider } from './context/Mocking';
 
@@ -9,12 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-      <ChakraProvider theme={theme}>
         <DatabaseProvider>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
           <App/>
         </DatabaseProvider>
-      </ChakraProvider>
 
   </React.StrictMode>
 );
