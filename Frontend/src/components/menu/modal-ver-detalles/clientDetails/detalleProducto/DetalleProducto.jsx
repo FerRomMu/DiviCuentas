ff5ea9793e2f8d53b1338ca0556502fbe836488a
Fernando Romero <ferrommu@gmail.com>
2023-10-08 14:08:26 -0300

    agrego estilos al detalle del pedido.

--- Frontend/src/components/menu/modal-ver-detalles/clientDetails/detalleProducto/DetalleProducto.jsx
+++ Frontend/src/components/menu/modal-ver-detalles/clientDetails/detalleProducto/DetalleProducto.jsx
@@ -1,7 +1,9 @@
+import './DetalleProducto.css'
+
 const DetalleProducto = ({product}) => {
 
     return (
-        <section>
+        <section className='detalle-producto'>
             <img src={ product.image }/>
             <p>{ product.name }</p>
             <p>{ product.amount }</p>
